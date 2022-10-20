f94cc6f56c4155d6ba78cb45d737d12c1e55fa05
Everson Leonardi <tibarisl@gmail.com>
2022-10-19 21:32:35 -0300

    Atualizações 19-10

--- Steps/Analise/Spacy/Spacy.py
+++ Steps/Analise/Spacy/Spacy.py
@@ -11,6 +11,9 @@ generate_training_file(filepath="G:\\Meu Drive\\TCC\\TCC II - Everson Leonardi\\
 
 
 #   Executar esta linha de comando no Terminal para rodar os testes.
-# python -m spacy train "G:\Meu Drive\TCC\TCC II - Everson Leonardi\Projeto\Dados\Spacy\config.cfg" --output "G:\Meu Drive\TCC\TCC II - Everson Leonardi\Projeto\Dados\Spacy\scispacy" --paths.train "G:\Meu Drive\TCC\TCC II - Everson Leonardi\Projeto\Dados\TrainFile\train.spacy" --paths.dev "G:\Meu Drive\TCC\TCC II - Everson Leonardi\Projeto\Dados\TrainFile\train.spacy"
+# Usando o mesmo arquivo .train para o --paths.dev
+# python -m spacy train "G:\Meu Drive\TCC\TCC II - Everson Leonardi\Projeto\Dados\Spacy\config.cfg" --output "G:\Meu Drive\TCC\TCC II - Everson Leonardi\Projeto\Dados\Spacy\models" --paths.train "G:\Meu Drive\TCC\TCC II - Everson Leonardi\Projeto\Dados\TrainFile\train.spacy" --paths.dev "G:\Meu Drive\TCC\TCC II - Everson Leonardi\Projeto\Dados\TrainFile\train.spacy"
+# Usando o arquivo .test para o --paths.dev
+# python -m spacy train "G:\Meu Drive\TCC\TCC II - Everson Leonardi\Projeto\Dados\Spacy\config.cfg" --output "G:\Meu Drive\TCC\TCC II - Everson Leonardi\Projeto\Dados\Spacy\models" --paths.train "G:\Meu Drive\TCC\TCC II - Everson Leonardi\Projeto\Dados\TrainFile\train.spacy" --paths.dev "G:\Meu Drive\TCC\TCC II - Everson Leonardi\Projeto\Dados\TrainFile\test.spacy"
 
 
